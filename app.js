aa52f69986a438d515edf31e64d3f8cc1052d93e
lohithavodnala <vodnalalohitha@gmail.com>
2025-04-16 16:26:55 -0500

    modified

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require('mongoose');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const createError = require('http-errors');
+require('dotenv').config();
 
+// Import routes
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var ornithologyRouter = require('./routes/ornithology');
 var gridRouter = require('./routes/grid');
 var pickRouter = require('./routes/pick');
+
+// Import the Ornithology model
 var Ornithology = require("./models/ornithology");
 
 var app = express();
@@ -28,6 +32,7 @@ app.use('/users', usersRouter);
 app.use('/ornithology', ornithologyRouter);
 app.use('/grid', gridRouter);
 app.use('/pick', pickRouter);
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
@@ -35,78 +40,58 @@ app.use(function (req, res, next) {
 
 // error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
 
 // MongoDB connection
-require('dotenv').config();
-const connectionString = process.env.MONGO_CON
-mongoose = require('mongoose');
-mongoose.connect(connectionString);
+const connectionString = process.env.MONGO_CON;
+mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
 
-
-
-//Get the default connection
 var db = mongoose.connection;
-//Bind connection to error event
+// Bind connection to error event
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.once("open", function () {
-  console.log("Connection to DB succeeded")
+  console.log("Connection to DB succeeded");
 });
 
-// We can seed the collection if needed onserver start
+// We can seed the collection if needed on server start
 async function recreateDB() {
-  // Delete everything
-  await ornithology.deleteMany();
-  let instance1 = new
-    ornithology({
-      ornithology_type: "ghost", size: 'large',
-      cost: 15.4
+  try {
+    // Delete everything
+    await Ornithology.deleteMany();
+
+    let instance1 = new Ornithology({
+      ornithology_type: "ghost", size: 'large', cost: 15.4
     });
-  instance1.save().then(doc => {
-    console.log("First object saved")
-  }
-  ).catch(err => {
-    console.error(err)
-  });
-  let instance2 = new
-    ornithology({
-      ornithology_type: "bird", size: 'small',
-      cost: 80
+    await instance1.save();
+    console.log("First object saved");
+
+    let instance2 = new Ornithology({
+      ornithology_type: "bird", size: 'small', cost: 80
     });
-  instance2.save().then(doc => {
-    console.log("Second object saved")
-  }
-  ).catch(err => {
-    console.error(err)
-  });
-  let instance3 = new
-    ornithology({
-      ornithology_type: "ghost", size: 'medium',
-      cost: 20
+    await instance2.save();
+    console.log("Second object saved");
+
+    let instance3 = new Ornithology({
+      ornithology_type: "ghost", size: 'medium', cost: 20
     });
-  instance3.save().then(doc => {
-    console.log("Third object saved")
+    await instance3.save();
+    console.log("Third object saved");
+  } catch (err) {
+    console.error("Error while seeding the database:", err);
   }
-  ).catch(err => {
-    console.error(err)
-  });
 }
+
 let reseed = true;
-if (reseed) { recreateDB(); }
-
-var express = require('express');
-var app = express();
-
-// your middlewares, routes, etc.
+if (reseed) { 
+  recreateDB(); 
+}
 
 module.exports = app;
+
 
 
 
