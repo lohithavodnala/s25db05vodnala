ab156033e23c95d2ed9f0cab1af8c292b195c909
lohithavodnala <vodnalalohitha@gmail.com>
2025-04-16 17:35:18 -0500

    added controllers

--- app.js
+++ app.js
@@ -1,18 +1,14 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const mongoose = require('mongoose');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const createError = require('http-errors');
-require('dotenv').config();
 
-// Import routes
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var ornithologyRouter = require('./routes/ornithology');
 var gridRouter = require('./routes/grid');
 var pickRouter = require('./routes/pick');
-
-// Import the Ornithology model
 var Ornithology = require("./models/ornithology");
 
 var app = express();
@@ -32,7 +28,6 @@ app.use('/users', usersRouter);
 app.use('/ornithology', ornithologyRouter);
 app.use('/grid', gridRouter);
 app.use('/pick', pickRouter);
-
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
@@ -40,58 +35,71 @@ app.use(function (req, res, next) {
 
 // error handler
 app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
 
 // MongoDB connection
-const connectionString = process.env.MONGO_CON;
-mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
+require('dotenv').config();
+const connectionString = process.env.MONGO_CON
+mongoose = require('mongoose');
+mongoose.connect(connectionString);
 
+
+
+//Get the default connection
 var db = mongoose.connection;
-// Bind connection to error event
+//Bind connection to error event
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.once("open", function () {
-  console.log("Connection to DB succeeded");
+  console.log("Connection to DB succeeded")
 });
 
-// We can seed the collection if needed on server start
+// We can seed the collection if needed onserver start
 async function recreateDB() {
-  try {
-    // Delete everything
-    await Ornithology.deleteMany();
-
-    let instance1 = new Ornithology({
-      ornithology_type: "ghost", size: 'large', cost: 15.4
+  // Delete everything
+  await ornithology.deleteMany();
+  let instance1 = new
+    ornithology({
+      ornithology_type: "ghost", size: 'large',
+      cost: 15.4
     });
-    await instance1.save();
-    console.log("First object saved");
-
-    let instance2 = new Ornithology({
-      ornithology_type: "bird", size: 'small', cost: 80
-    });
-    await instance2.save();
-    console.log("Second object saved");
-
-    let instance3 = new Ornithology({
-      ornithology_type: "ghost", size: 'medium', cost: 20
-    });
-    await instance3.save();
-    console.log("Third object saved");
-  } catch (err) {
-    console.error("Error while seeding the database:", err);
+  instance1.save().then(doc => {
+    console.log("First object saved")
   }
+  ).catch(err => {
+    console.error(err)
+  });
+  let instance2 = new
+    ornithology({
+      ornithology_type: "bird", size: 'small',
+      cost: 80
+    });
+  instance2.save().then(doc => {
+    console.log("Second object saved")
+  }
+  ).catch(err => {
+    console.error(err)
+  });
+  let instance3 = new
+    ornithology({
+      ornithology_type: "ghost", size: 'medium',
+      cost: 20
+    });
+  instance3.save().then(doc => {
+    console.log("Third object saved")
+  }
+  ).catch(err => {
+    console.error(err)
+  });
 }
-
 let reseed = true;
-if (reseed) { 
-  recreateDB(); 
-}
-
-module.exports = app;
-
+if (reseed) { recreateDB(); }
 
 
 
---
+++ controllers/ornithology.js
@@ -0,0 +1,70 @@
+const Ornithology = require('../models/ornithology'); // Adjust the path as needed
+
+// GET all ornithology records
+exports.ornithology_list = async function(req, res) {
+    try {
+        const result = await Ornithology.find();
+        res.send(result);
+    } catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    }
+};
+
+// GET one ornithology record by ID
+exports.ornithology_detail = async function(req, res) {
+    console.log("detail " + req.params.id);
+    try {
+        const result = await Ornithology.findById(req.params.id);
+        res.send(result);
+    } catch (error) {
+        res.status(500);
+        res.send(`{"error": document for id ${req.params.id} not found}`);
+    }
+};
+
+// POST a new ornithology record
+exports.ornithology_create_post = async function(req, res) {
+    console.log(req.body);
+    let document = new Ornithology();
+    document.bird_species = req.body.bird_species;
+    document.habitat = req.body.habitat;
+    document.region = req.body.region;
+
+    try {
+        let result = await document.save();
+        res.send(result);
+    } catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    }
+};
+
+// PUT update an ornithology record
+exports.ornithology_update_put = async function(req, res) {
+    console.log(`update on id ${req.params.id} with body ${JSON.stringify(req.body)}`);
+    try {
+        let toUpdate = await Ornithology.findById(req.params.id);
+
+        if (req.body.bird_species) toUpdate.bird_species = req.body.bird_species;
+        if (req.body.habitat) toUpdate.habitat = req.body.habitat;
+        if (req.body.region) toUpdate.region = req.body.region;
+
+        let result = await toUpdate.save();
+        res.send(result);
+    } catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}: Update for id ${req.params.id} failed}`);
+    }
+};
+
+// DELETE an ornithology record
+exports.ornithology_delete = async function(req, res) {
+    try {
+        const result = await Ornithology.findByIdAndDelete(req.params.id);
+        res.send(result);
+    } catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    }
+};
